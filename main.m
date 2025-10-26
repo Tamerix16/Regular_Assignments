2c3b6db408ad8f9c711cc103a2c7078e56c3c071
Tamerix16 <darcy.stewart2005@outlook.com>
2025-10-26 21:32:21 +1000

    added matlab for submission

--- main.m
+++ main.m
@@ -1,13 +1,63 @@
 clear all;
-m = 10; %kg
+m = 0.1; %kg
 theta_0 = 0.05; %rad
 theta_0_dot = 0.01; %rad/s
 l0 = 20; %meters
 r0 = 22; %meters
 r0_dot = 0.1; % m/s
 
-k = 10; %N/m
+k = 20; %N/m
 g = 9.8; %m/s2
+x(1) = 22*sin(0.05);
+y(1) = 22*cos(0.05);
+x_dot0 = r0_dot*sin(theta_0) + r0*theta_0_dot*cos(theta_0);
+y_dot0 = r0_dot*cos(theta_0) - r0*theta_0_dot*sin(theta_0);
+dt = 0.1;
+y(2) = y(1)+y_dot0*dt;
+x(2) = x(1)+x_dot0*dt;
+t_1 = 0:dt:10;
+for j = 2:length(t_1)-1
+    l = sqrt(x(j).^2+y(j).^2);
+    x_prev = x(j-1);
+    y_prev = y(j-1);
+    x_cur = x(j);
+    y_cur = y(j);
+    f_x = @(x_new)(m*((x_new-(2*x_cur)+x_prev) / (dt^2)) + k*(sqrt(x_cur^2+y_cur^2)-l0)*(x_cur/(sqrt(x_cur^2+y_cur^2))));
+    f_y = @(y_new)(m*((y_new-(2*y_cur)+y_prev) / (dt^2)) + k*(sqrt(x_cur^2+y_cur^2)-l0)*(y_cur/(sqrt(x_cur^2+y_cur^2)))-m*g);
+    x_new = fzero(f_x,x_cur);
+    y_new = fzero(f_y, y_cur);
+    x(j+1) = x_new;
+    y(j+1) = y_new;
+end
+
+figure
+plot(x,y)
+title('Path')
+xlabel('X (m)')
+ylabel('Y (m)')
+set(gca, 'YDir', 'reverse')
+figure
+subplot(1,3,1)
+plot(t_1,x)
+title('X Motion')
+ylabel('Amplitude (m)')
+xlabel('Time (s)')
+subplot(1,3,2)
+plot(t_1,y)
+title('Y Motion')
+xlabel('Time (s)')
+ylabel('Amplitude (m)')
+subplot(1,3,3)
+plot(x,y)
+title('Path')
+xlabel('X (m)')
+ylabel('Y (m)')
+set(gca, 'YDir', 'reverse')
+
+
+%% task 7
+k = 10;
+m= 10;
 x(1) = 22*sin(0.05);
 y(1) = 22*cos(0.05);
 x_dot0 = r0_dot*sin(theta_0) + r0*theta_0_dot*cos(theta_0);
@@ -29,11 +79,6 @@ for i = 2:length(t)-1
     x(i+1) = x_new;
     y(i+1) = y_new;
 end
-
-
-
-
-%% task 7
 
 f_theta = @(t)(0.052.*cos(0.7.*t-0.28));
 f_r = @(t)(7.8*cos(1.*t+0.000224)+29.8);
